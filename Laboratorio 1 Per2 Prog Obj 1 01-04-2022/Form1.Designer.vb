81403aaf55961f6f02c22283d5ede628cfb8b5d7
Jairo Orantes <jairo_02@outlook.es>
2022-04-01 22:56:30 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio 1 Per2 Prog Obj 1 01-04-2022/Form1.Designer.vb
@@ -0,0 +1,366 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1Producto1 = New System.Windows.Forms.TextBox()
+        Me.TextBox1PrecioProducto1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1Producto2 = New System.Windows.Forms.TextBox()
+        Me.TextBox2PrecioProducto2 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox1PrecioProducto3 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox2Producto3 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBoxSubtotal = New System.Windows.Forms.TextBox()
+        Me.TextBoxIVA = New System.Windows.Forms.TextBox()
+        Me.TextBoxDescuento = New System.Windows.Forms.TextBox()
+        Me.TextBoxtotal = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label11P1 = New System.Windows.Forms.Label()
+        Me.Label12P2 = New System.Windows.Forms.Label()
+        Me.Label13P3 = New System.Windows.Forms.Label()
+        Me.Label11pr1 = New System.Windows.Forms.Label()
+        Me.Label12pr2 = New System.Windows.Forms.Label()
+        Me.Label13pr3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Producto 1 :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(508, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Precio ($) :"
+        '
+        'TextBox1Producto1
+        '
+        Me.TextBox1Producto1.Location = New System.Drawing.Point(99, 12)
+        Me.TextBox1Producto1.Name = "TextBox1Producto1"
+        Me.TextBox1Producto1.Size = New System.Drawing.Size(387, 23)
+        Me.TextBox1Producto1.TabIndex = 2
+        '
+        'TextBox1PrecioProducto1
+        '
+        Me.TextBox1PrecioProducto1.Location = New System.Drawing.Point(569, 12)
+        Me.TextBox1PrecioProducto1.Name = "TextBox1PrecioProducto1"
+        Me.TextBox1PrecioProducto1.Size = New System.Drawing.Size(136, 23)
+        Me.TextBox1PrecioProducto1.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(508, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Sub Total :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(508, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 15)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Impuesto (IVA) :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(508, 206)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 15)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "TOTAL :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(508, 181)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(69, 15)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Descuento :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 44)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(71, 15)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Producto 2 :"
+        '
+        'TextBox1Producto2
+        '
+        Me.TextBox1Producto2.Location = New System.Drawing.Point(99, 41)
+        Me.TextBox1Producto2.Name = "TextBox1Producto2"
+        Me.TextBox1Producto2.Size = New System.Drawing.Size(387, 23)
+        Me.TextBox1Producto2.TabIndex = 11
+        '
+        'TextBox2PrecioProducto2
+        '
+        Me.TextBox2PrecioProducto2.Location = New System.Drawing.Point(569, 41)
+        Me.TextBox2PrecioProducto2.Name = "TextBox2PrecioProducto2"
+        Me.TextBox2PrecioProducto2.Size = New System.Drawing.Size(136, 23)
+        Me.TextBox2PrecioProducto2.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(508, 44)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(63, 15)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Precio ($) :"
+        '
+        'TextBox1PrecioProducto3
+        '
+        Me.TextBox1PrecioProducto3.Location = New System.Drawing.Point(569, 70)
+        Me.TextBox1PrecioProducto3.Name = "TextBox1PrecioProducto3"
+        Me.TextBox1PrecioProducto3.Size = New System.Drawing.Size(136, 23)
+        Me.TextBox1PrecioProducto3.TabIndex = 17
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(508, 73)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(63, 15)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Precio ($) :"
+        '
+        'TextBox2Producto3
+        '
+        Me.TextBox2Producto3.Location = New System.Drawing.Point(99, 70)
+        Me.TextBox2Producto3.Name = "TextBox2Producto3"
+        Me.TextBox2Producto3.Size = New System.Drawing.Size(387, 23)
+        Me.TextBox2Producto3.TabIndex = 15
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(22, 73)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(71, 15)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Producto 3 :"
+        '
+        'TextBoxSubtotal
+        '
+        Me.TextBoxSubtotal.Enabled = False
+        Me.TextBoxSubtotal.Location = New System.Drawing.Point(605, 127)
+        Me.TextBoxSubtotal.Name = "TextBoxSubtotal"
+        Me.TextBoxSubtotal.Size = New System.Drawing.Size(100, 23)
+        Me.TextBoxSubtotal.TabIndex = 18
+        '
+        'TextBoxIVA
+        '
+        Me.TextBoxIVA.Enabled = False
+        Me.TextBoxIVA.Location = New System.Drawing.Point(605, 152)
+        Me.TextBoxIVA.Name = "TextBoxIVA"
+        Me.TextBoxIVA.Size = New System.Drawing.Size(100, 23)
+        Me.TextBoxIVA.TabIndex = 19
+        '
+        'TextBoxDescuento
+        '
+        Me.TextBoxDescuento.Enabled = False
+        Me.TextBoxDescuento.Location = New System.Drawing.Point(605, 178)
+        Me.TextBoxDescuento.Name = "TextBoxDescuento"
+        Me.TextBoxDescuento.Size = New System.Drawing.Size(100, 23)
+        Me.TextBoxDescuento.TabIndex = 20
+        '
+        'TextBoxtotal
+        '
+        Me.TextBoxtotal.Enabled = False
+        Me.TextBoxtotal.Location = New System.Drawing.Point(605, 203)
+        Me.TextBoxtotal.Name = "TextBoxtotal"
+        Me.TextBoxtotal.Size = New System.Drawing.Size(100, 23)
+        Me.TextBoxtotal.TabIndex = 21
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(99, 206)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(387, 23)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(18, 206)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Salir"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label11P1
+        '
+        Me.Label11P1.AutoSize = True
+        Me.Label11P1.Location = New System.Drawing.Point(22, 111)
+        Me.Label11P1.Name = "Label11P1"
+        Me.Label11P1.Size = New System.Drawing.Size(20, 15)
+        Me.Label11P1.TabIndex = 24
+        Me.Label11P1.Text = "p1"
+        Me.Label11P1.Visible = False
+        '
+        'Label12P2
+        '
+        Me.Label12P2.AutoSize = True
+        Me.Label12P2.Location = New System.Drawing.Point(22, 130)
+        Me.Label12P2.Name = "Label12P2"
+        Me.Label12P2.Size = New System.Drawing.Size(20, 15)
+        Me.Label12P2.TabIndex = 25
+        Me.Label12P2.Text = "p2"
+        Me.Label12P2.Visible = False
+        '
+        'Label13P3
+        '
+        Me.Label13P3.AutoSize = True
+        Me.Label13P3.Location = New System.Drawing.Point(22, 150)
+        Me.Label13P3.Name = "Label13P3"
+        Me.Label13P3.Size = New System.Drawing.Size(20, 15)
+        Me.Label13P3.TabIndex = 26
+        Me.Label13P3.Text = "p3"
+        Me.Label13P3.Visible = False
+        '
+        'Label11pr1
+        '
+        Me.Label11pr1.AutoSize = True
+        Me.Label11pr1.Location = New System.Drawing.Point(360, 111)
+        Me.Label11pr1.Name = "Label11pr1"
+        Me.Label11pr1.Size = New System.Drawing.Size(24, 15)
+        Me.Label11pr1.TabIndex = 27
+        Me.Label11pr1.Text = "pr1"
+        Me.Label11pr1.Visible = False
+        '
+        'Label12pr2
+        '
+        Me.Label12pr2.AutoSize = True
+        Me.Label12pr2.Location = New System.Drawing.Point(360, 130)
+        Me.Label12pr2.Name = "Label12pr2"
+        Me.Label12pr2.Size = New System.Drawing.Size(24, 15)
+        Me.Label12pr2.TabIndex = 28
+        Me.Label12pr2.Text = "pr2"
+        Me.Label12pr2.Visible = False
+        '
+        'Label13pr3
+        '
+        Me.Label13pr3.AutoSize = True
+        Me.Label13pr3.Location = New System.Drawing.Point(360, 150)
+        Me.Label13pr3.Name = "Label13pr3"
+        Me.Label13pr3.Size = New System.Drawing.Size(24, 15)
+        Me.Label13pr3.TabIndex = 29
+        Me.Label13pr3.Text = "pr3"
+        Me.Label13pr3.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(718, 237)
+        Me.Controls.Add(Me.Label13pr3)
+        Me.Controls.Add(Me.Label12pr2)
+        Me.Controls.Add(Me.Label11pr1)
+        Me.Controls.Add(Me.Label13P3)
+        Me.Controls.Add(Me.Label12P2)
+        Me.Controls.Add(Me.Label11P1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBoxtotal)
+        Me.Controls.Add(Me.TextBoxDescuento)
+        Me.Controls.Add(Me.TextBoxIVA)
+        Me.Controls.Add(Me.TextBoxSubtotal)
+        Me.Controls.Add(Me.TextBox1PrecioProducto3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox2Producto3)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox2PrecioProducto2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox1Producto2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1PrecioProducto1)
+        Me.Controls.Add(Me.TextBox1Producto1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "COMPRA"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1Producto1 As TextBox
+    Friend WithEvents TextBox1PrecioProducto1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1Producto2 As TextBox
+    Friend WithEvents TextBox2PrecioProducto2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1PrecioProducto3 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox2Producto3 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBoxSubtotal As TextBox
+    Friend WithEvents TextBoxIVA As TextBox
+    Friend WithEvents TextBoxDescuento As TextBox
+    Friend WithEvents TextBoxtotal As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label11P1 As Label
+    Friend WithEvents Label12P2 As Label
+    Friend WithEvents Label13P3 As Label
+    Friend WithEvents Label11pr1 As Label
+    Friend WithEvents Label12pr2 As Label
+    Friend WithEvents Label13pr3 As Label
+End Class
---
+++ Laboratorio 1 Per2 Prog Obj 1 01-04-2022/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+
+    Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim producto1, producto2, producto3 As String
+        Dim precio1, precio2, precio3 As Double
+        producto1 = TextBox1Producto1.Text
+        producto2 = TextBox1Producto2.Text
+        producto3 = TextBox2Producto3.Text
+        precio1 = TextBox1PrecioProducto1.Text
+        precio2 = TextBox2PrecioProducto2.Text
+        precio3 = TextBox1PrecioProducto3.Text
+        TextBoxSubtotal.Text = Math.Round(subtotal(precio1, precio2, precio3), 2)
+        TextBoxIVA.Text = Math.Round(IVA(precio1, precio2, precio3), 2)
+        TextBoxDescuento.Text = Math.Round(descuento(precio1, precio2, precio3), 2)
+        TextBoxtotal.Text = Math.Round(total(precio1, precio2, precio3), 2)
+        mostrar(producto1, producto2, producto3, precio1, precio2, precio3)
+
+
+    End Sub
+    'CALCULAR SUBTOTAL
+    Function subtotal(precio1, precio2, precio3) As Double
+        Return precio1 + precio2 + precio3
+    End Function
+    'CALCULAR IVA
+    Function IVA(precio1, precio2, precio3) As Double
+        Return subtotal(precio1, precio2, precio3) * 0.13
+    End Function
+    'CALCULAR DESCUENTO
+    Function descuento(precio1, precio2, precio3) As Double
+        If subtotal(precio1, precio2, precio3) < 0 Then
+            Return MsgBox("Dato incorrecto")
+        Else
+            If subtotal(precio1, precio2, precio3) > 0 And subtotal(precio1, precio2, precio3) < 25 Then
+                Return 0
+            Else
+                If subtotal(precio1, precio2, precio3) > 25 And subtotal(precio1, precio2, precio3) < 100 Then
+                    Return subtotal(precio1, precio2, precio3) * 0.05
+                Else
+                    If subtotal(precio1, precio2, precio3) > 100 And subtotal(precio1, precio2, precio3) < 150 Then
+                        Return subtotal(precio1, precio2, precio3) * 0.1
+                    Else
+                        Return subtotal(precio1, precio2, precio3) * 0.15
+
+                    End If
+                End If
+            End If
+        End If
+    End Function
+    'CALCULAR TOTAL
+    Function total(precio1, precio2, precio3) As Double
+        Return (subtotal(precio1, precio2, precio3) - descuento(precio1, precio2, precio3)) + IVA(precio1, precio2, precio3)
+    End Function
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+
+    Private Sub TextBox1PrecioProducto1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1PrecioProducto1.TextChanged
+        If TextBox1PrecioProducto1.Text < 0 Then
+            MsgBox("Dato incorrecto")
+            TextBox1PrecioProducto1.Focus()
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub TextBox1PrecioProducto3_TextChanged(sender As Object, e As EventArgs) Handles TextBox1PrecioProducto3.TextChanged
+        If TextBox1PrecioProducto1.Text < 0 Then
+            MsgBox("Dato incorrecto")
+            TextBox1PrecioProducto1.Focus()
+        Else
+            Button1.Enabled = True
+        End If
+    End Sub
+
+    Sub mostrar(producto1 As String, producto2 As String, producto3 As String, precio1 As Double, precio2 As Double, precio3 As Double)
+        Label11P1.Visible = True
+        Label12P2.Visible = True
+        Label13P3.Visible = True
+        Label11pr1.Visible = True
+        Label12pr2.Visible = True
+        Label13pr3.Visible = True
+        Label11P1.Text = producto1
+        Label12P2.Text = producto2
+        Label13P3.Text = producto3
+        Label11pr1.Text = "$" & precio1
+        Label12pr2.Text = "$" & precio2
+        Label13pr3.Text = "$" & precio3
+
+    End Sub
+
+    Private Sub TextBox1Producto1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1Producto1.TextChanged
+        If TextBox1Producto1.Text = "" Then
+            MsgBox("Ingresa un dato")
+            TextBox1Producto1.Focus()
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+
+End Class
